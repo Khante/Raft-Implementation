93f54a787c8b90738dda0b8eb2dde320165dc811
Rohan Khante <happykhante@gmail.com>
2018-04-08 16:01:12 -0600

    kkk

--- attempt(1).py
+++ attempt(1).py
@@ -190,7 +190,8 @@ def leaderClient():
         print("True")
         for i in range(len(clientBindArray)):
             print("TooTrue")
-            message[i] = clientBindArray[i].recv_json().split(':')[1]
+            print(clientBindArray[i].recv_json())
+            #message[i] = clientBindArray[i].recv_json().split(':')[1]
         print(message[0])
         print(message[1])
         if message[0] == "blocking_with_left" and message[1] == "punch_with_right":
